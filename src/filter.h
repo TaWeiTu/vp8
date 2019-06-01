9e8fb0ead2f069d4f8eea060010c79418aeb9f50
ltf0501 <0110420@stu.nknush.kh.edu.tw>
2019-06-01 21:16:07 +0800

    add mb/sb filters, add Clamp fictions

--- src/filter.h
+++ src/filter.h
@@ -1,14 +1,10 @@
 #ifndef FILTER_H_
 #define FILTER_H_
 
-#include "utils.h" 
+#include "utils.h"
 
 namespace vp8 {
-	int16_t minus128(int16_t x) {
-		return Clamp128(x - 128); 
-	}
-	int16_t plus128(int16_t x) {
-		return Clamp255(x + 128);
-	}
-} // namespace vp8
-#endif // FILTER_H_
+int16_t minus128(int16_t x) { return Clamp128(x - 128); }
+int16_t plus128(int16_t x) { return Clamp255(x + 128); }
+}  // namespace vp8
+#endif  // FILTER_H_
--- src/utils.h
+++ src/utils.h
@@ -20,12 +20,12 @@ void ensure(bool, const std::string & = "");
 
 template <typename T>
 T abs(T x) {
-	return x < T(0) ? -x : x;
+  return x < T(0) ? -x : x;
 }
 
 template <typename T>
 T Clamp(T val, T from, T to) {
-	return val < from ? from : (val > to ? to : val);
+  return val < from ? from : (val > to ? to : val);
 }
 
 template <typename T>
@@ -35,7 +35,7 @@ T Clamp255(T x) {
 
 template <typename T>
 T Clamp128(T x) {
-	return Clamp(x, T(-128), T(127));
+  return Clamp(x, T(-128), T(127));
 }
 
 void ensure(bool cond, const std::string &message) {
