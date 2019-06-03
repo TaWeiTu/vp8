d27c1bff6159887a75b28dff3be254a601e77f9a
ltf0501 <0110420@stu.nknush.kh.edu.tw>
2019-06-03 23:08:39 +0800

    nothing

--- src/decode.cc
+++ src/decode.cc
@@ -6,7 +6,7 @@ void Reconstruct(const FrameHeader &header, Frame &frame) {
   auto &mh = header.macroblock_header;
   for (size_t r = 0; r < frame.vblock; ++r) {
     for (size_t c = 0; c < frame.hblock; ++c) {
-      if (mh[r][c].is_inter_mb) InterPredict(header, r, c, frame);
+      if (mh.at(r).at(c).is_inter_mb) InterPredict(header, r, c, frame);
       else InterPredict(header, r, c, frame);
 
       // TODO: Dequantize the DCT coefficients and perform inverse DCT to get the residuals. 
