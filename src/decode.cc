29a7efbc5f155c935009c9d8ab70f2ebe3733fe2
TaWeiTu <tu.da.wei@gmail.com>
2019-06-06 01:33:35 +0800

    add InitSignBias() and RefreshRefFrames()

--- src/decode.cc
+++ src/decode.cc
@@ -1,14 +1,49 @@
 #include <array>
 
+#include "bitstream_const.h"
 #include "reconstruct.h"
+
+void InitSignBias(const vp8::FrameHeader &header,
+                  std::array<bool, 4> &ref_frame_bias) {
+  ref_frame_bias[vp8::GOLDEN_FRAME] = header.sign_bias_golden;
+  ref_frame_bias[vp8::ALTREF_FRAME] = header.sign_bias_alternate;
+}
+
+void RefreshRefFrames(const vp8::FrameHeader &header,
+                      std::array<vp8::Frame, 4> &ref_frames,
+                      const vp8::Frame &frame) {
+  if (header.refresh_golden_frame) {
+    ref_frames[vp8::GOLDEN_FRAME] = frame;
+  } else {
+    if (header.copy_buffer_to_golden == 1)
+      ref_frames[vp8::GOLDEN_FRAME] = ref_frames[vp8::LAST_FRAME];
+    else if (header.copy_buffer_to_golden == 2)
+      ref_frames[vp8::GOLDEN_FRAME] = ref_frames[vp8::ALTREF_FRAME];
+  }
+  if (header.refresh_alternate_frame) {
+    ref_frames[vp8::ALTREF_FRAME] = frame;
+  } else {
+    if (header.copy_buffer_to_alternate == 1)
+      ref_frames[vp8::ALTREF_FRAME] = ref_frames[vp8::LAST_FRAME];
+    else if (header.copy_buffer_to_alternate == 2)
+      ref_frames[vp8::ALTREF_FRAME] = ref_frames[vp8::GOLDEN_FRAME];
+  }
+  if (header.refresh_last) ref_frames[vp8::LAST_FRAME] = frame;
+}
 
 int main(int argc, const char **argv) {
   // Avoid unused function.
+  std::array<vp8::Frame, 4> ref_frames;
+  std::array<bool, 4> ref_frame_bias;
+
+  // Decoding loop: reconstruct the frame and update the golden/altref frame (if
+  // necessary).
   vp8::FrameHeader header;
   vp8::FrameTag tag;
-  std::array<vp8::Frame, 4> refs;
-  std::array<bool, 4> ref_frame_bias;
   vp8::BitstreamParser ps;
   vp8::Frame frame;
-  vp8::Reconstruct(header, tag, refs, ref_frame_bias, ps, frame);
+
+  InitSignBias(header, ref_frame_bias);
+  vp8::Reconstruct(header, tag, ref_frames, ref_frame_bias, ps, frame);
+  RefreshRefFrames(header, ref_frames, frame);
 }
